3ead91413d1ba0db88bb49da868f5d7e0b68dad9
dwsmi <dwsmile30@gmail.com>
2026-05-26 13:52:11 +0900

    feat: 민원 목록 보기 구현

---
+++ src/main/java/com/campus/controller/ComplaintController.java
@@ -0,0 +1,27 @@
+package com.campus.controller;
+
+import com.campus.dto.ComplaintDTO;
+import com.campus.service.ComplaintService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/complaints")
+public class ComplaintController extends HttpServlet {
+    private final ComplaintService complaintService = new ComplaintService();
+
+    // GET 요청
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+                                            throws ServletException, IOException {
+        List<ComplaintDTO> complaints = complaintService.findComplaintList();
+        req.setAttribute("complaints", complaints);
+        req.getRequestDispatcher("/WEB-INF/views/test/list.jsp").forward(req, res);
+        // req.getRequestDispatcher("/WEB-INF/views/complaint/list.jsp").forward(req, res);
+
+    }
+}
--- src/main/java/com/campus/service/ComplaintService.java
+++ src/main/java/com/campus/service/ComplaintService.java
@@ -1,4 +1,16 @@
 package com.campus.service;
 
+import com.campus.dao.ComplaintDAO;
+import com.campus.dto.ComplaintDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ComplaintService {
+
+    private final ComplaintDAO complaintDAO =  new ComplaintDAO();
+
+    public List<ComplaintDTO> findComplaintList() {
+        return complaintDAO.findAll();
+    }
 }
