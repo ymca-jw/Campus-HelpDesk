643074b8d7a7dfb0ff24dc8b9533cc96e78729b8
dwsmi <dwsmile30@gmail.com>
2026-05-26 14:08:37 +0900

    feat: controller 추가

--- src/main/java/com/campus/controller/ComplaintController.java
+++ src/main/java/com/campus/controller/ComplaintController.java
@@ -10,14 +10,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet("/complaints")
+@WebServlet({"/complaints", "/complaints/detail", "/complaint/new", "/complaint/new", "/complaints/check",
+"/complaints/create"})
 public class ComplaintController extends HttpServlet {
     private final ComplaintService complaintService = new ComplaintService();
 
     // GET 요청
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res)
-                                            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         List<ComplaintDTO> complaints = complaintService.findComplaintList();
         req.setAttribute("complaints", complaints);
         req.getRequestDispatcher("/WEB-INF/views/test/list.jsp").forward(req, res);
