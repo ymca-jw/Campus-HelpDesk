9d7bd8394a938b882b97a0fa10f1370eafc432a3
endnjs09 <dwsmile30@gmail.com>
2026-06-02 18:09:42 +0900

    feat: 공통 카테고리 유틸리티

---
+++ src/main/java/com/campus/util/CategoryConstants.java
@@ -0,0 +1,11 @@
+package com.campus.util;
+
+import java.util.List;
+
+public class CategoryConstants {
+
+    private CategoryConstants() {}
+
+    public static final List<String> CATEGORIES =
+            List.of( "전산", "시설", "교무학적", "장학", "학생지원", "수업", "상담", "취업", "인권", "기타");
+}
